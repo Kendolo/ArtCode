a774e9eb02956a463d892ae84c6c7ba640c5f6db
letuk <51926694+letuk@users.noreply.github.com>
2021-09-23 00:03:02 +0200

    code generator

--- Program.cs
+++ Program.cs
@@ -13,15 +13,25 @@ namespace ArtCode
         {
             Bitmap image = new Bitmap("D:\\Projects\\GitHub\\ArtCode\\img1.bmp");
             int[,] imageMatrix = new int[image.Width, image.Height];
+            int[] code = new int[image.Width * image.Height];
+            int index = 0;
 
             for(int i = 0; i < image.Height; i++)
             {
                 for(int j = 0; j < image.Width; j++)
                 {
                     imageMatrix[i, j] = Gray2Binary(image.GetPixel(j,i));
+                    code[index] = imageMatrix[i, j];
+                    index++;
                     Console.Write(imageMatrix[i, j]);
                 }
                 Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < code.Length; i++)
+            {
+                Console.Write(code[i]);
             }
             Console.Read();
         }
