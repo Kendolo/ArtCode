423d501fb1b32be2b3866a58d537e3737eea2f26
letuk <51926694+letuk@users.noreply.github.com>
2021-09-22 23:52:52 +0200

    Basic image reader and binary converter

--- Program.cs
+++ Program.cs
@@ -11,7 +11,32 @@ namespace ArtCode
     {
         static void Main(string[] args)
         {
-            Bitmap image = null;
+            Bitmap image = new Bitmap("D:\\Projects\\GitHub\\ArtCode\\img1.bmp");
+            int[,] imageMatrix = new int[image.Width, image.Height];
+
+            for(int i = 0; i < image.Height; i++)
+            {
+                for(int j = 0; j < image.Width; j++)
+                {
+                    imageMatrix[i, j] = Gray2Binary(image.GetPixel(j,i));
+                    Console.Write(imageMatrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
+
+        static int Gray2Binary(Color color)
+        {
+            if(color.GetBrightness() > 0.5f)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
     }
 }
