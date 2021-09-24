25c2c9da71c3af77755dd6020420d5407e6182ce
Kendolo <52171294+Kendolo@users.noreply.github.com>
2021-09-24 16:56:16 +0200

    Save Image + Method Clean Up

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ArtCode
 {
@@ -14,74 +15,76 @@ namespace ArtCode
         static void Main(string[] args)
         {
             OpenFileDialog open = new OpenFileDialog();
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            LoadImage(open);
 
-            if (open.ShowDialog() == DialogResult.OK)
-            {
-                Bitmap image = new Bitmap(open.FileName);
+            Bitmap oldImage = new Bitmap(open.FileName);
+            Bitmap newImage = Convert(oldImage);
 
-                int[,] imageMatrix = new int[image.Width, image.Height];
-                int[] code = new int[image.Width * image.Height];
-                int index = 0;
-
-                for (int i = 0; i < image.Height; i++)
-                {
-                    for (int j = 0; j < image.Width; j++)
-                    {
-                        imageMatrix[i, j] = Gray2Binary(image.GetPixel(j, i));
-                        code[index] = imageMatrix[i, j];
-                        index++;
-                        Console.Write(imageMatrix[i, j]);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-
-                for (int i = 0; i < code.Length; i++)
-                {
-                    Console.Write(code[i]);
-                }
-
-                Bitmap newImage = new Bitmap(image.Width, image.Height);
-
-                for (int i = 0; i < image.Height; i++)
-                {
-                    for (int j = 0; j < image.Width; j++)
-                    {
-                        Color black = Color.Black;
-                        Color white = Color.White;
-
-                        if(imageMatrix[i,j] == 0)
-                        {
-                            newImage.SetPixel(j, i, black);
-                        }
-                        else
-                        {
-                            newImage.SetPixel(j, i, white);
-                        }
-                    }
-                }
-
-                Image img = (Image)newImage;
-                String newPath = "D:\\Projects\\GitHub\\ArtCode\\Resources\\newImg1.bmp";
-                img.Save(newPath);
-                System.Diagnostics.Process.Start(newPath);
-
-                Console.Read();
-            }
+            SaveImage((Image)newImage);
         }
 
-        static int Gray2Binary(Color color)
+        static void LoadImage(OpenFileDialog openInMethod)
         {
-            if(color.GetBrightness() > 0.5f)
+            openInMethod.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+            if (openInMethod.ShowDialog() != DialogResult.OK)
             {
-                return 1;
-            }
-            else
-            {
-                return 0;
+                Console.WriteLine("Error");
+                Application.Exit();
             }
         }
 
+        static Bitmap Convert(Bitmap oldImage)
+        {
+            Bitmap newImage = oldImage;
+            Color black = Color.Black;
+            Color white = Color.White;
+            int[] code = new int[oldImage.Width * oldImage.Height];
+            int index = 0;
+
+            for (int i = 0; i < oldImage.Height; i++)
+            {
+                for (int j = 0; j < oldImage.Width; j++)
+                {
+                    if (oldImage.GetPixel(j, i).GetBrightness() > 0.5f)
+                    {
+                        newImage.SetPixel(j, i, white);
+                        code[index] = 1;
+                    }
+                    else
+                    {
+                        newImage.SetPixel(j, i, black);
+                        code[index] = 0;
+                    }                  
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                Console.Write(code[i]);
+            }
+
+            return newImage;
+        }
+
+        static void SaveImage(Image newImage)
+        {
+            Stream newImageStream = new MemoryStream();
+            SaveFileDialog save = new SaveFileDialog();
+
+            save.Filter = "BMP Image | *.bmp";
+            save.RestoreDirectory = true;
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                if ((newImageStream = save.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    newImage.Save(newImageStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                    newImageStream.Close();
+                }
+            }
+        }
     }
 }
