2b3c81ec52f80e48cda4c93323ba1cbde4f5e7e3
Kendolo <52171294+Kendolo@users.noreply.github.com>
2021-09-26 01:56:30 +0200

    Marker found

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
+using System.Numerics;
 
 namespace ArtCode
 {
@@ -20,7 +21,7 @@ namespace ArtCode
             Bitmap oldImage = new Bitmap(open.FileName);
             Bitmap newImage = Convert(oldImage);
 
-            SaveImage((Image)newImage);
+            //SaveImage((Image)newImage);
 
             //Find marker using newImage
             Scanner(newImage);
@@ -99,9 +100,20 @@ namespace ArtCode
             int colorCounter = 0;
 
             int[] ratios = new int[5];
+            List<Vector2> horizontalPositions = new List<Vector2>();
+            List<Vector2> verticalPositions = new List<Vector2>();
+            List<Vector2> matchingPositions = new List<Vector2>();
+            List<List<Vector2>> markerPositionGroup = new List<List<Vector2>>();
+            List<Vector2> markerPositions = new List<Vector2>();
 
-            for(int i = 0; i < imageToBeChecked.Height; i++)
+            for (int i = 0; i < imageToBeChecked.Height; i++)
             {
+                ratios[0] = 0;
+                ratios[1] = 0;
+                ratios[2] = 0;
+                ratios[3] = 0;
+                ratios[4] = 0;
+
                 for (int j = 0; j < imageToBeChecked.Width; j++)
                 {
                     currentColor = imageToBeChecked.GetPixel(j, i);
@@ -115,26 +127,115 @@ namespace ArtCode
                         ratios[1] = ratios[0];
                         ratios[0] = colorCounter;
                         colorCounter = 0;
-                        //Console.WriteLine();
-                        //Console.Write(ratios[6] + " : " + ratios[5] + " : " + ratios[4] + " : " + ratios[3] + " : " + ratios[2] + " : " + ratios[1] + " : " + ratios[0]);
 
-                        if(ratios[4] == 1 && ratios[3] == 1 && ratios[2] == 3 && ratios[1] == 1 && ratios[0] == 1)
+                        if(ratios[4] == 1 && ratios[3] == 1 && ratios[2] == 3 && ratios[1] == 1 && ratios[0] == 1 && currentColor == Color.FromArgb(255, 255, 255, 255))
                         {
-                            Console.WriteLine("Possible marker in line " + (i+1) + " and column " + (j+1) + "!");
-                            //Console.Read();
+                            Console.WriteLine("Possible marker (horizontal): ");
+                            for (int p = 4; p > 1; p--)
+                            {
+                                horizontalPositions.Add(new Vector2(i + 1, j - p));
+                                Console.WriteLine((i + 1) + ", " + (j - p));
+                            }
                         }
-                    }
-
-                    if(j == 0) //imageToBeChecked.Width - 1)
-                    {
-                        ratios[0] = 0;
-                        ratios[1] = 0;
-                        ratios[2] = 0;
-                        ratios[3] = 0;
-                        ratios[4] = 0;
                     }
                     colorCounter++;
                 }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < imageToBeChecked.Width; i++)
+            {
+                ratios[0] = 0;
+                ratios[1] = 0;
+                ratios[2] = 0;
+                ratios[3] = 0;
+                ratios[4] = 0;
+
+                for (int j = 0; j < imageToBeChecked.Height; j++)
+                {
+                    currentColor = imageToBeChecked.GetPixel(i, j);
+
+                    if (currentColor != previousColor)
+                    {
+                        previousColor = currentColor;
+                        ratios[4] = ratios[3];
+                        ratios[3] = ratios[2];
+                        ratios[2] = ratios[1];
+                        ratios[1] = ratios[0];
+                        ratios[0] = colorCounter;
+                        colorCounter = 0;
+
+                        if (ratios[4] == 1 && ratios[3] == 1 && ratios[2] == 3 && ratios[1] == 1 && ratios[0] == 1 && currentColor == Color.FromArgb(255, 255, 255, 255))
+                        {
+                            Console.WriteLine("Possible marker (vertical): ");
+                            for (int p = 4; p > 1; p--)
+                            {
+                                verticalPositions.Add(new Vector2(j - p, i + 1));
+                                Console.WriteLine((j - p) + ", " + (i + 1));
+                            }
+                        }
+                    }
+                    colorCounter++;
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < horizontalPositions.Count(); i++)
+            {
+                for (int j = 0; j < verticalPositions.Count(); j++)
+                {
+                    if (horizontalPositions[i] == verticalPositions[j])
+                    {
+                        matchingPositions.Add(horizontalPositions[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            markerPositionGroup.Add(new List<Vector2>());
+            markerPositionGroup[0].Add(matchingPositions[0]);
+            bool passtRein = false;
+
+            for (int i = 1; i < matchingPositions.Count(); i++)
+            {
+                Console.WriteLine("Matching position: " + matchingPositions[i]);
+
+                for (int j = 0; j < markerPositionGroup.Count(); j++)
+                {
+                    if (Math.Abs(matchingPositions[i].X - markerPositionGroup[j][0].X) < 3 && Math.Abs(matchingPositions[i].Y - markerPositionGroup[j][0].Y) < 3)
+                    {
+                        markerPositionGroup[j].Add(matchingPositions[i]);
+                        passtRein = true;
+                    }
+                }
+
+                if (!passtRein)
+                {
+                    markerPositionGroup.Add(new List<Vector2>());
+                    markerPositionGroup[markerPositionGroup.Count - 1].Add(matchingPositions[i]);
+                }
+                passtRein = false;
+            }
+
+            for (int i = 0; i < markerPositionGroup.Count(); i++)
+            {
+                float xSum = 0;
+                float ySum = 0;
+                for (int j = 0; j < markerPositionGroup[i].Count(); j++)
+                {
+                    Console.WriteLine(markerPositionGroup[i][j]);
+                    xSum += markerPositionGroup[i][j].X;
+                    ySum += markerPositionGroup[i][j].Y;
+                }
+                markerPositions.Add(new Vector2(xSum / markerPositionGroup[i].Count(), ySum / markerPositionGroup[i].Count()));
+            }
+
+            for (int i = 0; i < markerPositions.Count(); i++)
+            {
+                Console.WriteLine("Marker at: " + markerPositions[i]);
             }
         }
     }
